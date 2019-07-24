c5e49b2dcbf1694254c8f26bc8196ca2a6321a1f
vmehrotra50 <varun.mehrotra50@gmail.com>
2019-07-23 22:37:40 -0400

    Initial commit

---
+++ src/main/java/frc/robot/commands/DriveArcade.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class DriveArcade extends Command {
+  public DriveArcade() {
+    requires(Robot.m_drivetrain);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    double moveSpeed = Robot.m_oi.driverController.getRawAxis(5);
+    double rotateSpeed = Robot.m_oi.driverController.getRawAxis(2);
+
+    Robot.m_drivetrain.drive(moveSpeed, rotateSpeed);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.m_drivetrain.drive(0, 0);
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+
+/**
+ * Add your docs here.
+ */
+public class Drivetrain extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  private Talon leftTalon;
+  private Talon rightTalon;
+  private DifferentialDrive diffDrive;
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public Drivetrain() {
+    leftTalon = new Talon(RobotMap.LEFT_TALON_PORT);
+    rightTalon = new Talon(RobotMap.RIGHT_TALON_PORT);
+
+    diffDrive = new DifferentialDrive(leftTalon, rightTalon);
+  }
+
+  public void drive(double moveSpeed, double rotateSpeed) {
+    diffDrive.arcadeDrive(moveSpeed, rotateSpeed);
+  }
+}
