ad72e9ca7ae613214a5dc3560a254f8126785625
vmehrotra50 <varun.mehrotra50@gmail.com>
2019-07-25 10:08:48 -0400

    yeet

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -41,6 +41,7 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
+    System.out.println("yeet");
   }
 
   /**
