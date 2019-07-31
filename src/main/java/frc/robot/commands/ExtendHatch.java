4213c72d69abedef33314fbb945f0adfb3d7e91e
vmehrotra50 <varun.mehrotra50@gmail.com>
2019-07-31 10:28:40 -0400

    Added HatchSubsystem

--- src/main/java/frc/robot/commands/ExtendHatch.java
+++ src/main/java/frc/robot/commands/ExtendHatch.java
@@ -8,26 +8,21 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.Robot;
 
-public class DriveArcade extends Command {
-  public DriveArcade() {
-    requires(Robot.m_drivetrain);
+public class ExtendHatch extends Command {
+  public ExtendHatch() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-
-  } 
+  }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    double moveSpeed = Robot.m_oi.driverController.getRawAxis(5);
-    double rotateSpeed = Robot.m_oi.driverController.getRawAxis(2);
-
-    Robot.m_drivetrain.drive(moveSpeed, rotateSpeed);
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -39,13 +34,11 @@ public class DriveArcade extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
-    Robot.m_drivetrain.drive(0, 0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
-    end();
   }
 }
--- src/main/java/frc/robot/subsystems/HatchSubsystem.java
+++ src/main/java/frc/robot/subsystems/HatchSubsystem.java
@@ -1,7 +1,40 @@
-public class HatchSubsystem {
-    public HatchSubsystem() {
-        //
-        //
-        //
-    }
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+
+/**
+ * Add your docs here.
+ */
+public class HatchSubsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  DoubleSolenoid hatchSolenoid = new DoubleSolenoid(RobotMap.HATCH_SOLENOID_PORT1, RobotMap.HATCH_SOLENOID_PORT2);
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public void extendHatch() {
+    hatchSolenoid.set(DoubleSolenoid.Value.kForward);
+  }
+
+  public void retractHatch() {
+    hatchSolenoid.set(DoubleSolenoid.Value.kReverse);
+  }
+
+  public void turnOffHatch() {
+    hatchSolenoid.set(DoubleSolenoid.Value.kOff);
+  }
 }
